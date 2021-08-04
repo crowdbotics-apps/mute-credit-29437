7ad617eab4ded4c7fecbd7e1dfd6d965c341dc99
Crowdbotics <team@crowdbotics.com>
2021-08-04 02:28:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "mute_credit_29437Identifier",
+  url: "https://mute_credit_29437Identifier.botics.co",
+  api: "https://mute_credit_29437Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
